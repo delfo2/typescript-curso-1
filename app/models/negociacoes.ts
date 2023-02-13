6db23c4b4795568893f27d6ad844ee40d8d260d7
Carlos Eduardo <105376328+delfo2@users.noreply.github.com>
2023-02-13 13:08:21 -0300

    a asdsad

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -1,24 +1,8 @@
-export class Negociacao {
-    private _data;
-    private _quantidade;
-    private _valor;
+import { Negociacao } from "./negociacao.js";
 
-    constructor(data: Date, quantidade: number, valor: number) {
-        this._data = data;
-        this._quantidade = quantidade;
-        this._valor = valor
-    }
-    get data(): Date {
-        return this._data;
-    }
-    get quantidade(): number {
-        return this._quantidade;
-    }
-    get valor(): number {
-        return this._valor;
-    }
-
-    get volume(): number {
-        return this._quantidade * this._valor;
-    }
+export class Negociacoes {
+    private negociacoes: Array<Negociacao> = [];
 }
+
+const negociacoes = new Negociacoes();
+negociacoes.adiciona(negociacoes);
